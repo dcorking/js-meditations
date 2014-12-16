e63e2ad6a888580fd1b81f058f00def2ec955dce
David Corking <dmc-source@dcorking.com>
2014-12-16 11:06:33 +0000

    Update FizzBuzz to v2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,11 @@
-// Version 1, Fizz, buzz but not fizzbuzz.
-// SPOILER WARNING -
+// Version 2, Fizz, Buzz and FizzBuzz
+// SPOILER WARNING
 // Eloquent Javascript Chapter 2
 for (var n = 1; n <= 100; n++) {
   console.log (function () {
-    if (n % 3 == 0)
+    if (n % 3 == 0 && n% 5 == 0)
+      return "FizzBuzz"
+    else if (n % 3 == 0)
       return "Fizz"
     else if (n % 5 == 0)
       return "Buzz"
