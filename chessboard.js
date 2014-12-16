e7b394dff32a347825bb4c3c0c142164db5f4eb2
David Corking <dmc-source@dcorking.com>
2014-12-16 11:47:58 +0000

    Give chessboard a variable (even) size

--- chessboard.js
+++ chessboard.js
@@ -1,4 +1,6 @@
-// Chess board version 1 - 8x8 hard coded
+// Chess board version 2 - size is an argument
+// bug : gives an even width board for odd size
+
 // SPOILER WARNING
 // A solution to Eloquent Javascript Chapter 2
 
@@ -6,13 +8,14 @@
 // require ("./chessboard.js")
 // console.log(board)
 
-var board = function () {
+var board = function (size) {
   var str = "";
-  for (var i = 0; i < 8 ; i++){
-    for (var j = 0; j < 8 ; j = j + 2){
+  for (var i = 0; i < size ; i++){
+    for (var j = 0; j < size ; j = j + 2){
+      console.log("j = " + j + ". str = " + str)
       str += (i%2 == 0 ? "# " : " #")
     }
     str += "\n"
   }
   return str;
-}()
+}(21)
