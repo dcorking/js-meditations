c50f50c644a729ebf4aa277eb001f752afa79057
David Corking <dmc-source@dcorking.com>
2014-12-16 17:55:51 +0000

    Fix chessboard so it works on odd and even sizes

--- chessboard.js
+++ chessboard.js
@@ -1,21 +1,22 @@
 // Chess board version 2 - size is an argument
-// bug : gives an even width board for odd size
 
 // SPOILER WARNING
 // A solution to Eloquent Javascript Chapter 2
 
 // To use:
-// require ("./chessboard.js")
+// Set the board size on line 23
+// > eval (fs.readFileSync('./chessboard.js') + '');
+// undefined
+// > require ("./chessboard.js")
 // console.log(board)
 
 var board = function (size) {
   var str = "";
   for (var i = 0; i < size ; i++){
-    for (var j = 0; j < size ; j = j + 2){
-      console.log("j = " + j + ". str = " + str)
-      str += (i%2 == 0 ? "# " : " #")
+    for (var j = 0; j < size ; j++){
+      str += ((i+j)%2 == 0 ? "#" : " ");
     }
-    str += "\n"
+    str += "\n";
   }
   return str;
-}(21)
+}(21);
