fd60c106ceeb36d594bac7f62dee8981fd0ea6eb
David Corking <dmc-source@dcorking.com>
2015-01-23 15:22:30 +0000

    Eloquent Javascript Ch 3 - complete count-beans.js

--- count-beans.js
+++ count-beans.js
@@ -1,25 +1,34 @@
 // count-beans.js - two character counting functions
-// countBs version 1 - self sufficient
+// countBs version 2, uses countChar
 
 // SPOILER WARNING
 // A solution to Eloquent Javascript Chapter 3
 
 // countBs(str) - number of upper case B's in string str
 var countBs = function (s) {
+  return countChar(s, "B");
+  }
+
+// countChar(str, c) - number of characters, c,in string str
+var countChar = function (s, c) {
   // recursive implementation because it is fun and easy,
   // even though it fills up the stack
   var len = s.length;
-  var capB; // counts any capital B at the end of the string
+  var capChar; // counts any c at the end of the string
   if (len == 0)
     return 0;
-  if (s.charAt(len -1) == "B")
-    capB = 1
+  if (s.charAt(len -1) == c)
+    capChar = 1;
   else
-    capB = 0
-  return capB + countBs(s.substr(0, s.length - 1));
-  }
+    capChar = 0;
+  return capChar + countChar(s.substr(0, s.length - 1), c);
+}
 
+// tests
 if (countBs("BABBAGE") !=3) console.log("BABBAGE FAILED")
-if (countBs("HOPPER") !=0) console.log("HOPPER FAILED")
+if (countBs("HOPPER") !=0) console.log("HOPPER (B) FAILED")
 if (countBs("Bernard Cribbins") !=1) console.log("Bernard Cribbins FAILED")
+if (countChar("LOVELACE", "L") !=2) console.log("LOVELACE FAILED")
+if (countChar("HOPPER", "L") !=0) console.log("HOPPER (L) FAILED")
+if (countChar("Lendl", "L") !=1) console.log("Lendl FAILED")
 console.log ("tests completed.")
