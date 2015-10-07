b7a185c87b18a52caaff3a7de658b028b6f19077
David Corking <dmc-source@dcorking.com>
2015-10-07 20:03:31 +0100

    breakout: blue stroked rectangle

--- breakout-game.js
+++ breakout-game.js
@@ -1,4 +1,16 @@
-var BG = {} // global container for game code
+// Copyright Chris Mills
+// https://developer.mozilla.org/en-US/profiles/chrisdavidmills,
+// Mozilla Development Network and individual MDN contributors.
+// Arbitrarily modified by David Corking.
+
+// Unless otherwise indicated, the content is available under the
+// terms of the Creative Commons Attribution-ShareAlike license
+// (CC-BY-SA), v2.5 or any later version.
+// http://creativecommons.org/licenses/by-sa/2.5/ --> Made by
+// following the tutorial at
+// https://developer.mozilla.org/en-US/docs/Games/Workflows/2D_Breakout_game_pure_JavaScript
+
+var BG = {}; // global container for game code
 
 BG.canvas = document.getElementById("myCanvas");
 BG.ctx = BG.canvas.getContext("2d");
@@ -13,11 +25,7 @@ BG.redSquare = function (ctx){
 
 BG.redSquare(BG.ctx);
 
-// draw a green disc immediately
-// and in an overly fussy style: do I really need
-// an immediately invoked anonymous function?
-// I am just glad I got the parens in the right place
-// to get this to compile
+// draw a green disc with an anonymous function?
 (function (ctx) {
   ctx.beginPath();
   ctx.arc(240, 160, 20, 0, Math.PI*2, false);
@@ -25,3 +33,10 @@ BG.redSquare(BG.ctx);
   ctx.fill();
   ctx.closePath();
   })(BG.ctx);
+
+// empty rectangle
+BG.ctx.beginPath();
+BG.ctx.rect(160, 10, 100, 40);
+BG.ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+BG.ctx.stroke();
+BG.ctx.closePath();
