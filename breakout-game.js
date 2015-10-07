217894b24be1fa05efeebf3865cb38d72175cbfa
David Corking <dmc-source@dcorking.com>
2015-10-07 20:28:53 +0100

    breakout: repainting a static ball

--- breakout-game.js
+++ breakout-game.js
@@ -15,28 +15,17 @@ var BG = {}; // global container for game code
 BG.canvas = document.getElementById("myCanvas");
 BG.ctx = BG.canvas.getContext("2d");
 
-BG.redSquare = function (ctx){
+BG.ball = {};
+BG.ball.draw = function (ctx){
   ctx.beginPath();
-  ctx.rect(20, 40, 50, 50);
-  ctx.fillStyle = "#FF0000";
+  ctx.arc(50, 50, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095dd"
   ctx.fill();
   ctx.closePath();
-  };
+  }
 
-BG.redSquare(BG.ctx);
+BG.draw = function () {
+  BG.ball.draw(BG.ctx);
+  }
 
-// draw a green disc with an anonymous function?
-(function (ctx) {
-  ctx.beginPath();
-  ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-  ctx.fillStyle= "green";
-  ctx.fill();
-  ctx.closePath();
-  })(BG.ctx);
-
-// empty rectangle
-BG.ctx.beginPath();
-BG.ctx.rect(160, 10, 100, 40);
-BG.ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-BG.ctx.stroke();
-BG.ctx.closePath();
+setInterval(BG.draw, 10);
