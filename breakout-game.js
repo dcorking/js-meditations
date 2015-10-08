96e984fe5cef089ca5eecca7bc16e2291243f3a7
David Corking <dmc-source@dcorking.com>
2015-10-08 20:31:08 +0100

    breakout: ball bounces off walls, but is prone
    to getting stuck on the wall if it ever overshoots

--- breakout-game.js
+++ breakout-game.js
@@ -21,26 +21,48 @@ BG.ball = {
   y: BG.canvas.height - 30,
   // velocity
   dx: 2,
-  dy: -2
+  dy: -2,
+  radius: 10
 };
 
 BG.ball.draw = function (ctx) {
   ctx.beginPath();
-  ctx.arc(this.x, this.y, 10, 0, Math.PI*2);
+  ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD"
   ctx.fill();
   ctx.closePath();
-}
+  console.log(this);
+};
 
 BG.ball.step = function () {
+  // collision with top - TODO poss. refactor to avoid referring
+  // directly to BG ?
+  // TODO ball is prone to getting stuck in the corners
+  if (this.y - this.radius + this.dy < 0 ||
+      this.y + this.radius + this.dy > BG.canvas.height ) {
+        this.dy = - this.dy;
+      };
+  // collision with sides
+  if (this.x - this.radius + this.dx < 0 ||
+      this.x + this.radius + this.dy > BG.canvas.width ) {
+    this.dx = - this.dx;
+  };
   this.x += this.dx;
   this.y += this.dy;
-}
+};
 
+// Draw the world, then update it
 BG.draw = function () {
+  // When setInterval invokes BG.draw, 'this' is the global object
+  // so this doesn't refer to my game object
+  // When I wrap the callback in setInterval to an anonymous function, so:
+  //    function(){BG.draw()}
+  // then 'this' does indeed become the game object.
   BG.ctx.clearRect(0, 0, BG.canvas.width, BG.canvas.height);
   BG.ball.draw(BG.ctx);
   BG.ball.step()
-}
+};
 
-setInterval(BG.draw, 10);
+setInterval(
+  function(){BG.draw()},
+  30);
