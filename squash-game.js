f56bbab7347be864ee10ea47cfa0212cb3c1a516
David Corking <dmc-source@dcorking.com>
2016-05-02 19:50:44 +0100

    squash: minor cleanup, mostly comments

--- squash-game.js
+++ squash-game.js
@@ -1,13 +1,12 @@
-// Copyright Chris Mills
-// https://developer.mozilla.org/en-US/profiles/chrisdavidmills,
+// Copyright 2015 David Corking, Chris Mills,
 // Mozilla Development Network and individual MDN contributors.
-// Arbitrarily modified by David Corking.
 
 // Unless otherwise indicated, the content is available under the
 // terms of the Creative Commons Attribution-ShareAlike license
 // (CC-BY-SA), v2.5 or any later version.
-// http://creativecommons.org/licenses/by-sa/2.5/ --> Made by
-// following the tutorial at
+// http://creativecommons.org/licenses/by-sa/2.5/
+
+// Adapted from the tutorial at
 // https://developer.mozilla.org/en-US/docs/Games/Workflows/2D_Breakout_game_pure_JavaScript
 
 // ********************************
@@ -55,7 +54,7 @@ sg.ball = {
 //initialize game
 sg.score = 0;
 sg.paused = false;
-sg.gameOver = false;
+sg.gameOver = false; // the game never ends
 // initialize ball
 sg.ball.initialize = function () {
   // velocity
@@ -81,7 +80,7 @@ sg.ball.draw = function (ctx) {
 
 sg.ball.step = function () {
   // TODO poss. refactor to avoid referring
-  // directly to sg ?
+  // directly to sg
   //////////////////////////////
   // collision with top
   if (this.y - this.radius + this.dy < 0) {
@@ -101,7 +100,7 @@ sg.ball.step = function () {
   };
   // lose the ball and lose a point
   if (this.y + this.radius + this.dy > sg.canvas.height ) {
-    sg.score -= 1
+    sg.score -= 1;
     // TODO pause
 
     // restart ball
