d3b427928269eeff0df1be4fa7cd056551d22a7a
David Corking <dmc-source@dcorking.com>
2016-05-25 07:25:08 +0100

    wealth-builder: retirement models, initial commit

---
+++ wealth-builder.js
@@ -0,0 +1,7 @@
+interestRate = 15  // in basis points, that is 15 is 1.5%
+marginalTaxRate = 200 // 20%
+nationalInsuranceRate = 0 // not in model
+
+annualContribution = 300 // in pounds, before tax
+
+yearsParticipation = 20 // years paying into plan
