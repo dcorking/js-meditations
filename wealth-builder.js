a9f5df37138afcb1dfcaa5b183f253a54e956034
David Corking <dmc-source@dcorking.com>
2016-05-25 08:25:39 +0100

    wealth-builder: add a basic compound interest func

--- wealth-builder.js
+++ wealth-builder.js
@@ -5,3 +5,19 @@ nationalInsuranceRate = 0 // not in model
 annualContribution = 300 // in pounds, before tax
 
 yearsParticipation = 20 // years paying into plan
+
+// simple interest, contribution at start of year
+oneYearOutput = function (principal, contribution, interestRate) {
+  return (principal + contribution) *  (1000 + interestRate) / 1000;
+}
+
+// compound interest, contribution at start of year, interest paid at
+// end of year
+nYearsOutput = function (n, principal, contribution, interestRate) {
+  var sum = principal; // running total
+  for(var i=0; i<n; i++) {
+    sum = oneYearOutput(sum, contribution, interestRate);
+    console.log(sum, i);
+  };
+  return sum;
+}
