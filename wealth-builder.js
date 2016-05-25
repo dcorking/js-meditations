4564e42b82e633f7c811bfca6a189cdbd174e6cb
David Corking <dmc-source@dcorking.com>
2016-05-25 19:02:32 +0100

    wealth-builder: add isa and pension calculations

--- wealth-builder.js
+++ wealth-builder.js
@@ -1,10 +1,12 @@
-interestRate = 15  // in basis points, that is 15 is 1.5%
-marginalTaxRate = 200 // 20%
-nationalInsuranceRate = 0 // not in model
+// Coyright dcorking, 2016
+// MIT License and disclaimer of warranty
+interestRate = 15;  // in basis points, that is 15 is 1.5%
+marginalTaxRate = 200; // 20%
+nationalInsuranceRate = 0; // not in model
 
-annualContribution = 300 // in pounds, before tax
+annualContribution = 300; // in pounds, before tax
 
-yearsParticipation = 20 // years paying into plan
+yearsParticipation = 20; // years paying into plan
 
 // simple interest, contribution at start of year
 oneYearOutput = function (principal, contribution, interestRate) {
@@ -20,4 +22,20 @@ nYearsOutput = function (n, principal, contribution, interestRate) {
     console.log(sum, i);
   };
   return sum;
-}
+};
+
+// isa-style saving: pay tax on income before save it, don`t pay tax
+// on growth or withdrawals
+isaSaving = function (n, principal, contribution, interestRate) {
+  return nYearsOutput(n,
+                      principal,
+                      contribution * (1000 - marginalTaxRate) / 1000,
+                      interestRate);
+};
+
+// pension-style saving: save pre-tax income, grows tax free,
+// but pay income tax on withdrawals
+pensionSaving = function (n, principal, contribution, interestRate) {
+  untaxedSum = nYearsOutput(n, principal, contribution, interestRate);
+  return untaxedSum * (1000 - marginalTaxRate) / 1000;
+};
